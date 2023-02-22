08e46614f8c2c61ab11a28fe4da9dd90a7f232fe
Varsha Gopal <vgopal2@ncsu.edu>
2023-02-21 19:34:59 -0500

    add course tab to instructor manage course view

---
+++ src/plugin_files/manage_course/docs.php
@@ -0,0 +1,10 @@
+<div class="stm_lms_course__file_attachment">
+	<?php
+	STM_LMS_Templates::show_lms_template(
+		'manage_course/forms/course_files',
+		array(
+			'field_key' => 'course_files',
+		)
+	)
+	?>
+</div>
